7bf05332770d83362144e60e0ec871621898d20a
Xing Zhou <xing@xingzhou.me>
2012-09-30 15:00:25 -0400

    Add basic text POST requests in upload

--- request_handler.js
+++ request_handler.js
@@ -1,4 +1,4 @@
-var exec = require("child_process").exec;
+var querystring = require("querystring");
 
 function start(response) {
   console.log("Request handler 'start' was called.");
@@ -20,10 +20,10 @@ function start(response) {
   response.end();
 }
 
-function upload(response) {
+function upload(response, postData) {
   console.log("Request handler 'upload' was called.");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello Upload");
+  response.write("You've sent: " + querystring.parse(postData).text);
   response.end();
 }
 
--- server.js
+++ server.js
@@ -5,10 +5,20 @@ var http = require("http")
 function start(route, handle) {
   // Asynchronous callback function to handle requests
   function onRequest(request, response) {
-    var pathname = url.parse(request.url).pathname;
+    var postData = ""
+    ,   pathname = url.parse(request.url).pathname;
     console.log("Request for" + pathname + " recieved.");
 
-    route(handle, pathname, response);
+    request.setEncoding("utf8");
+
+    request.addListener("data", function(postDataChunk) {
+      postData += postDataChunk;
+      console.log("Recieved POST data chunk '" + postDataChunk + "'.");
+    });
+
+    request.addListener("end", function() {
+      route(handle, pathname, response, postData);                
+    });
   }
 
   http.createServer(onRequest).listen(8888);
